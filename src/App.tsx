de838983a7aa179e7f8bc62e335a14fd2a6ad497
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2023-09-29 05:07:33 -0300

    style: defaultLayout and routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Routes'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
-      <h1>Hello, world!</h1>
     </ThemeProvider>
   )
 }
