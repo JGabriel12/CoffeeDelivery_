c2687a31963c80845c277f5487a2204ca3118efc
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2023-10-16 04:26:27 -0300

    feat: form validation

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,27 @@
-import { InputHTMLAttributes } from 'react'
-import { InputStyleContainer } from './style'
+import { forwardRef, InputHTMLAttributes } from 'react'
+import { RegularText } from '../Typography'
+import {
+  InputWrapper,
+  InputStyleContainer,
+  InputStyled,
+  RightText
+} from './style'
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>
-
-export function Input({ ...props }: InputProps) {
-  return <InputStyleContainer {...props} />
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  error?: string
+  rightText?: string
 }
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, rightText, className, ...props }, ref) => {
+    return (
+      <InputWrapper className={className}>
+        <InputStyleContainer hasError={!!error}>
+          <InputStyled ref={ref} {...props} />
+          {rightText && <RightText>{rightText}</RightText>}
+        </InputStyleContainer>
+        {error && <RegularText size="s">{error}</RegularText>}
+      </InputWrapper>
+    )
+  }
+)
